bbf9e473159fb25007bb83ffe5be9591266bc837
Victor Dantas <99653338+victormcdantas02@users.noreply.github.com>
2025-11-19 21:13:29 -0300

    Add files via upload

---
+++ my-app/App.js
@@ -0,0 +1,19 @@
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AppNavigator from "./src/navigation/AppNavigator";
+import { AuthProvider } from "./src/context/AuthContext";
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <AuthProvider>
+          <StatusBar style="light" />
+          <AppNavigator />
+        </AuthProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
